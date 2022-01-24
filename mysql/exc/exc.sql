b0485c0ebe42ed0fcd47842fec1be6d01e1ab547
rhdee <rhdee@naver.com>
2022-01-24 11:52:13 +0900

    0

---
+++ mysql/exc/exc.sql
@@ -0,0 +1,27 @@
+use world;
+
+show tables;
+
+-- 주석
+
+-- 테이블 생성
+
+
+create table member (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdatd datetime
+
+);
+
+create table member2 (
+	seq int,
+    name varchar(100),
+    id varchar(100),
+    regdatd datetime
+
+);
+
+
+
